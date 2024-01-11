a1ae9c87066cad2a14bcf173b38e08e2965d9c2f
toha-web <t.info@ukr.net>
2024-01-11 11:59:12 +0200

    Add to packer rotate function

--- src/utils/packer.js
+++ src/utils/packer.js
@@ -1,34 +1,56 @@
-export class Packer{
-  constructor(w, h){
-    this.root = { x: 0, y: 0, w: w, h: h };
-  }
-
-  fit(blocks){
-    let node;
-    for(let i = 0; i < blocks.length; i++){
-      const block = blocks[i];
-      if(node = this.findNode(this.root, block.w, block.h)){
-        block.fit = this.splitNode(node, block.w, block.h);
-      }        
+export class Packer {
+    constructor(w, h) {
+        this.root = { x: 0, y: 0, w, h };
     }
-  }
 
-  findNode(root, w, h){
-    if(root.used){
-      return this.findNode(root.right, w, h) || this.findNode(root.down, w, h);
-    }      
-    else if ((w <= root.w) && (h <= root.h)){
-      return root;
-    }      
-    else{
-      return null;
+    rotate(blockW, blockH){
+      let oldW = blockW;
+      let oldH = blockH;
+      return {blockW: oldH, blockH: oldW};
     }
-  }
 
-  splitNode(node, w, h){
-    node.used = true;
-    node.down = { x: node.x, y: node.y + h, w: node.w, h: node.h - h };
-    node.right = { x: node.x + w, y: node.y, w: node.w - w, h: h};
-    return node;
-  }
+    fit(blocks) {
+        let node;
+        for (let i = 0; i < blocks.length; i++) {
+            const block = blocks[i];
+            const rotateW = this.rotate(block.w, block.h).blockW;
+            const rotateH = this.rotate(block.w, block.h).blockH;
+            if ((node = this.findNode(this.root, block.w, block.h))) {
+                block.fit = this.splitNode(node, block.w, block.h);
+            }
+            else if((node = this.findNode(this.root, rotateW, rotateH))){
+                block.fit = this.splitNode(node, block.w = rotateW, block.h = rotateH);
+            }
+        }
+    }
+
+    findNode(root, blockW, blockH) {
+        if (root.used) {
+            return (
+                this.findNode(root.right, blockW, blockH) ||
+                this.findNode(root.down, blockW, blockH)
+            );
+        } else if (blockW <= root.w && blockH <= root.h) {
+            return root;
+        } else {
+            return null;
+        }
+    }
+
+    splitNode(node, blockW, blockH) {
+        node.used = true;
+        node.down = {
+            x: node.x,
+            y: node.y + blockH,
+            w: node.w,
+            h: node.h - blockH,
+        };
+        node.right = {
+            x: node.x + blockW,
+            y: node.y,
+            w: node.w - blockW,
+            h: blockH,
+        };
+        return node;
+    }
 }
